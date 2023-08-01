9633065b61b7f9abc42420260e5e4d2326b44bce
Matthew Vance <mvance@efuse.io>
2023-07-31 22:21:02 -0400

    add space keymap

--- home/.config/nvim/lua/core/keymaps.lua
+++ home/.config/nvim/lua/core/keymaps.lua
@@ -2,6 +2,7 @@ local s = vim.keymap.set
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+s({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 s("i", "jj", "<Esc>", { desc = "Go to Normal mode" })
 
