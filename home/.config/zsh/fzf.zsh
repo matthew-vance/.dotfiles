64f6652cb89ba50893c9fe4809fb7c406165bee4
Matthew Vance <matthew.vance@taillight.com>
2024-05-22 08:26:55 -0400

    make sure fzf works as expected

--- home/.config/zsh/fzf.zsh
+++ home/.config/zsh/fzf.zsh
@@ -1,8 +1,18 @@
 #!/usr/bin/env zsh
 
+source $ZDOTDIR/fzf/key-bindings.zsh
+source $ZDOTDIR/fzf/completion.zsh
+
 # Preview file content using bat (https://github.com/sharkdp/bat)
 export FZF_CTRL_T_OPTS="
   --preview 'bat -n --color=always {}'
   --bind 'ctrl-/:change-preview-window(down|hidden|)'"
+
+export FZF_CTRL_R_OPTS="
+  --preview 'echo {}' --preview-window up:3:hidden:wrap
+  --bind 'ctrl-/:toggle-preview'
+  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+  --color header:italic
+  --header 'Press CTRL-Y to copy command into clipboard'"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
