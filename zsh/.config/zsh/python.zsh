40235688b20d7574002adc80af5b3f19aa9be312
Matthew Vance <mavance44@gmail.com>
2024-07-21 16:17:21 -0400

    remove unneeded command check during python config

--- zsh/.config/zsh/python.zsh
+++ zsh/.config/zsh/python.zsh
@@ -4,6 +4,6 @@
 
 if command -v pyenv &> /dev/null; then
   export PYENV_ROOT="$HOME/.pyenv"
-  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+  export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
 fi
