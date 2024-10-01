b37cdd3e44681f9c5cdbc1fdb1442ce0ef52d43d
Matthew Vance <mavance44@gmail.com>
2024-09-30 21:15:02 -0400

    git config updates

--- configure-git.sh
+++ configure-git.sh
@@ -6,3 +6,7 @@ git config --global include.path "~/.config/delta/delta.gitconfig"
 
 git config --global core.pager "delta"
 git config --global interactive.diffFilter "delta --color-only --features=interactive"
+git config --global diff.colorMoved default
+git config --global push.autoSetupRemote true
+git config --global init.defaultBranch main
+git config --global pull.rebase false
