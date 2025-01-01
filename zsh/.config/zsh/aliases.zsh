b7ed5b4c1f2a5662571d8a55defe3c3ed8ee90f7
Matthew Vance <mavance44@gmail.com>
2025-01-01 12:03:51 -0500

    add yazi config

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -66,3 +66,13 @@ alias love="/Applications/love.app/Contents/MacOS/love"
 rop() {
     lsof -i -P | rg LISTEN | rg :$1
 }
+
+# Yazi shell wrapper
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		builtin cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
